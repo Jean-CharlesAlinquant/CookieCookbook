31b3e70381910ea7c489826a49e67e347e6f512c
Jean-CharlesAlinquant <jcalinquant@hotmail.com>
2024-01-26 17:26:35 +0000

    Adding Global try-catch block

--- Program.cs
+++ Program.cs
@@ -4,18 +4,30 @@ using CookieCookbook.FileAccess;
 using CookieCookbook.Recipes;
 using CookieCookbook.Recipes.Ingredients;
 
-const FileFormat Format = FileFormat.Json;
-IStringsRepository stringsRepository = Format == FileFormat.Json ?
-    new StringJsonRepository() :
-    new StringsTextualRepository();
+try
+{
+    const FileFormat Format = FileFormat.Json;
+    IStringsRepository stringsRepository = Format == FileFormat.Json ?
+        new StringJsonRepository() :
+        new StringsTextualRepository();
 
-const string fileName = "recipes";
-var fileMetaData = new FileMetadata(fileName, Format);
+    const string fileName = "recipes";
+    var fileMetaData = new FileMetadata(fileName, Format);
 
-var ingredientsRegister = new IngredientsRegister();
+    var ingredientsRegister = new IngredientsRegister();
 
-var cookiesRecipesApp = new CookiesRecipesApp(
-    new RecipesRepository(stringsRepository, ingredientsRegister),
-    new RecipesConsoleUserInteraction(ingredientsRegister)
-);
-cookiesRecipesApp.Run(fileMetaData.ToPath());
+    var cookiesRecipesApp = new CookiesRecipesApp(
+        new RecipesRepository(stringsRepository, ingredientsRegister),
+        new RecipesConsoleUserInteraction(ingredientsRegister)
+    );
+    cookiesRecipesApp.Run(fileMetaData.ToPath());
+}
+catch (Exception ex)
+{
+    Console.WriteLine("Sorry! The application experienced" +
+        " an unexpected error and will have to be closed. " +
+        "The error message: " + ex.Message);
+
+    Console.WriteLine("Press any key to close.");
+    Console.ReadKey();
+}
